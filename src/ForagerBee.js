7a75bb22f0814d225e9418eeefee83ed4b88f8ff
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-05-20 13:43:08 -0700

    Passed all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -7,7 +7,7 @@ var ForagerBee = function() {
 
 };
 ForagerBee.prototype = Object.create(Bee.prototype);
-ForagerBee.prototype.constructor = 'ForagerBee';
+ForagerBee.prototype.constructor = ForagerBee;
 ForagerBee.prototype.forage = function(treasure) {
   this.treasureChest.push(treasure);
 };
